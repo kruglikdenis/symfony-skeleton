7b64152fdf9cf103c35ccea2d0bc83abad3c26af
Kruglik Denis <kruglikdenis1995@gmail.com>
2018-06-01 11:07:48 +0300

    update to symfony 4.1, remove nelmio

--- config/bundles.php
+++ config/bundles.php
@@ -7,20 +7,15 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Fesor\RequestObject\Bundle\RequestObjectBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
-
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
-    BornFree\TacticianDomainEventBundle\TacticianDomainEventBundle::class => ['all' => true]
+    BornFree\TacticianDomainEventBundle\TacticianDomainEventBundle::class => ['all' => true],
 ];
